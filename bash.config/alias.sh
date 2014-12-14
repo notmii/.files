d72e6925fc60d0fd96e007df2e6cf058e26028e7
notmii <jrlizaso_antispam-public@yahoo.com>
2014-12-14 10:25:49 +0800

    Removed unused aliases. Removed installation of eclim in install-submodules.

--- bash.config/alias.sh
+++ bash.config/alias.sh
@@ -1,46 +1,3 @@
-search() {
-    grep --color=always -srn --exclude-dir=public/build --exclude=tags --exclude=cscope.out $1 .; find -name $1 -type f
-}
-
-update-production() {
-    HAS_CHANGES=$(git status --porcelain 2>/dev/null|egrep "^( M|M)"|wc -l)
-    [[ $HAS_CHANGES > 0 ]] && git stash -u
-
-    CURRENT_BRANCH=$(git branch|grep "^*"|awk '{print $2}')
-    git checkout production
-    git reset --hard production/master
-
-    echo "Updating CTAGS"
-    find -type f  \( -name '*.php' \)                   \
-        -and -not \( -path "./scripts/build-assets/*"   \
-            -o -path "./public/build/*" \)              \
-        > php.files
-
-    ctags -R --fields=+aimS --languages=php -f 'php.tags' -L 'php.files' --totals > /dev/null
-
-    rm php.files
-
-    find -type f  \(                                    \
-        -name '*.js'                                    \
-        -o -name '*.htm'                                \
-        -o -name '*.html' \)                            \
-        -and -not \( -path "./scripts/build-assets/*"   \
-            -o -path "./public/build/**/*"              \
-            -o -name '*.min.js'                         \
-            -o -name '*-min.js' \)                      \
-        > javascript.files
-
-    # ctags -R --fields=+afikKlmnsSzt --languages=javascript -f 'javascript.tags' -L 'javascript.files' --totals > /dev/null
-    ctags -R --languages=javascript -f 'javascript.tags' -L 'javascript.files' --totals > /dev/null
-
-    rm javascript.files
-
-    git checkout $CURRENT_BRANCH
-    [[ $HAS_CHANGES > 0 ]] && git stash pop
-    clear
-    git status
-}
-
 nginx-restart() {
     if [[ -f /run/nginx.pid ]]; then
         sudo kill $(cat /run/nginx.pid)
@@ -64,5 +21,5 @@ alias las='ls -ah'
 alias composer='composer -v'
 alias gr='noglob grep -ns'
 alias rgr='noglob rgrep -ns --exclude-dir public/build --exclude *.tags --exclude-dir public/tests'
-alias tmux='tmux attach'
+alias amux='tmux attach'
 alias httpserver='python -m SimpleHTTPServer'
