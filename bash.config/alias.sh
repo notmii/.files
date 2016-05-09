16d3e444c0c0265701c5d66c22bfeb898ae41e6b
notmii <jrlizaso_antispam-public@yahoo.com>
2016-05-10 05:59:43 +0800

    Added in zsh plugins vi-mode and zsh_reload

--- bash.config/alias.sh
+++ bash.config/alias.sh
@@ -1,8 +1,7 @@
 alias lah='la -h'
 alias las='ls -ah'
+alias v="vim"
 alias composer='composer -v'
-alias gr='noglob grep -ns'
-alias rgr='noglob rgrep -ns'
 alias amux='tmux attach'
 alias httpserver='python -m SimpleHTTPServer'
 alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
