32b1e9659391cb868dd500e5b069ed85b76f814b
notmii <jrlizaso_antispam-public@yahoo.com>
2016-07-28 09:39:03 +0800

    Can't describe enough

--- bash.config/alias.sh
+++ bash.config/alias.sh
@@ -4,4 +4,5 @@ alias v="vim"
 alias composer='composer -v'
 alias amux='tmux attach'
 alias httpserver='python -m SimpleHTTPServer'
-alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
+# alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
+alias gstu='git status -uno'
