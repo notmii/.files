8627af07eeedf81ba59b7de151dce8ead46b704b
notmii <jrlizaso_antispam-public@yahoo.com>
2016-08-02 01:09:44 +0800

    VI with FZF.

--- bash.config/alias.sh
+++ bash.config/alias.sh
@@ -6,3 +6,23 @@ alias amux='tmux attach'
 alias httpserver='python -m SimpleHTTPServer'
 # alias gl='git log --graph --pretty=format:'\''%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'\'' --abbrev-commit --all'
 alias gstu='git status -uno'
+
+vi() {
+    if [[ $# > 0 ]]; then
+        vim $@;
+    elif [[ $(which fzf) ]]; then
+
+        FILES="";
+
+        if [ -n "$TMUX" ]; then
+            FILES=$(fzf-tmux -m);
+        else
+            FILES=$(fzf -m);
+        fi
+
+        if [ $FILES ]; then
+            vim $FILES;
+        fi
+
+    fi
+}
